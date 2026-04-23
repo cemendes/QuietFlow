674ee166683790b1e3f70ed69b0b33f569061169
Eduardo Oliveira <cemolive@google.com>
2026-04-23 16:22:06 -0500

    Initial Commit

---
+++ FocusFlow/FocusFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusFlowApp.swift
+//  FocusFlow
+//
+//  Created by Eduardo Oliveira on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct FocusFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
